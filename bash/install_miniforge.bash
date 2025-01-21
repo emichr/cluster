c582357983e094e7e2b54b0d80550ebd79d632d7
Emil Christiansen <emil.christiansen@ntnu.no>
2025-01-21 16:05:09 +0100

    Update install_miniforge.bash

--- bash/install_miniforge.bash
+++ bash/install_miniforge.bash
@@ -7,3 +7,5 @@ cd ~/miniforge #Navigate into the new directory
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh" #Download the latest miniforge installer
 chmod 755 ~/miniforge/Miniforge3-$(uname)-$(uname -m).sh #Make installer executable
 bash ~/miniforge/Miniforge3-$(uname)-$(uname -m).sh #Run the installer
+echo "alias myminiforge='source ~/miniforge3/bin/activate'" >> ~/.bashrc
+source ~/.bashrc
